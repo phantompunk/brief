24bc857f7cf4b4a8e60272422025da7e269d9eeb
Rodrigo Moran <rmoran@ipponusa.com>
2021-02-28 10:10:07 -0500

    Add version command

--- command/command.go
+++ command/command.go
@@ -20,57 +20,10 @@ Options:
   --output 	Output directory for newly created report. Default is current directory.
 `
 
-var versionUsage = `Print the app version and build info for the current context.
-
-Usage: brief version [options...]
-
-Options:
-  --short  If true, print just the version number. Default false.
-`
-
-var (
-	build   = "???"
-	version = "???"
-)
-
 type Command interface {
 	Init()
 	Run()
 	Called()
-}
-
-type VersionCommand struct {
-	short bool
-	fs    *flag.FlagSet
-}
-
-func (c *VersionCommand) Init(args []string) error {
-	return c.fs.Parse(args)
-}
-
-func (c *VersionCommand) Run() {
-	if c.short {
-		fmt.Printf("brief version: v%s", version)
-	} else {
-		fmt.Printf("brief version: v%s, build: %s", version, build)
-	}
-	os.Exit(0)
-}
-
-func (c *VersionCommand) Called() bool {
-	return c.fs.Parsed()
-}
-
-func NewVersionCommand() *VersionCommand {
-	cmd := &VersionCommand{
-		fs: flag.NewFlagSet("version", flag.ExitOnError),
-	}
-
-	cmd.fs.BoolVar(&cmd.short, "short", false, "")
-	cmd.fs.Usage = func() {
-		fmt.Fprint(os.Stderr, fmt.Sprintf(versionUsage))
-	}
-	return cmd
 }
 
 type EditCommand struct {
